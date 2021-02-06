8474aaa13eb237226d7ada29dabcc764e4d15c97
dryan8a@gmail.com <dryan8a@gmail.com>
2021-02-05 16:53:20 -0800

    Realized I should work on an insert function before I decide to make the report function as I wouldn't be able to test it. Implemented Append and concat. Still need to test though

--- TextJustificationForms/Rope.cs
+++ TextJustificationForms/Rope.cs
@@ -14,15 +14,18 @@ namespace TextJustificationForms
             public Node RightChild;
             public string StrFrag;
             public int Weight;
+            public bool IsLeafNode { get; internal set; }
 
-            public Node(int Weight)
+            public Node(int Weight, bool IsLeafNode)
             {
                 this.Weight = Weight;
+                this.IsLeafNode = IsLeafNode;
             }
             public Node(int Weight, string StrFrag)
             {
                 this.Weight = Weight;
                 this.StrFrag = StrFrag;
+                IsLeafNode = true;
             }
         }
 
@@ -30,6 +33,12 @@ namespace TextJustificationForms
         
         public Rope()
         {
+            Head = new Node(0,false);
+        }
+
+        internal Rope(int headWeight)
+        {
+            Head = new Node(headWeight, false); 
         }
 
         public char this[int index]
@@ -55,45 +64,76 @@ namespace TextJustificationForms
             return node.StrFrag[index];
         }
 
-        public string Report(int startIndex, int endIndex)
-        {
-            Node startNode = Head;
-            int currentIndex = startIndex;
-            while(true)
-            {
-                if (startNode.Weight <= currentIndex && startNode.RightChild != null)
-                {
-                    startNode = startNode.RightChild;
-                    startIndex -= startNode.Weight;
-                    continue;
-                }
+        //public string Report(int startIndex, int endIndex)
+        //{
+        //    Node currentNode = Head;
+        //    int currentIndex = startIndex;
+        //    while(true)
+        //    {
+        //        if (currentNode.Weight >= endIndex && currentNode.Weight <= currentIndex && currentNode.RightChild != null)
+        //        {
+        //            currentNode = currentNode.RightChild;
+        //            startIndex -= currentNode.Weight;
+        //            continue;
+        //        }
 
-                if (startNode.LeftChild != null)
-                {
-                    startNode = startNode.LeftChild;
-                    continue;
-                }
+        //        if (currentNode.Weight >= endIndex && currentNode.LeftChild != null)
+        //        {
+        //            currentNode = currentNode.LeftChild;
+        //            continue;
+        //        }
+        //        break;
+        //    }
 
-                break;
-            }
+        //    Stack<Node> stack = new Stack<Node>();
+        //    while(currentNode != null || stack.Count > 0)
+        //    {
+        //        while(currentNode != null)
+        //        {
+        //            stack.Push(currentNode);
+        //            currentNode = currentNode.LeftChild;
+        //        }
 
-            //Do Inorder Traversal here starting at startNode
-            throw new NotImplementedException();
-        }
+        //        currentNode = stack.Pop();
+        //        if(currentNode.StrFrag != null )
+
+        //        currentNode = currentNode.RightChild;
+        //    }
+        //}
 
         public void Insert(string stringToInsert,int position)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException();  
         }
 
         public void Append(string stringToInsert)
         {
-            throw new NotImplementedException();
+            Insert(stringToInsert, Head.Weight);
         }
 
-        private Node Concat(Node left,Node right)
+        private static void InorderTraversal(Node startNode, List<Node> nodes)
         {
-            throw new NotImplementedException();
+            if (startNode == null) return;
+
+            InorderTraversal(startNode.LeftChild, nodes);
+            nodes.Add(startNode);
+            InorderTraversal(startNode.RightChild, nodes);
+        }
+
+        private static Rope Concat(Rope left,Rope right)
+        {
+            var inorderNodes = new List<Node>();
+            InorderTraversal(left.Head,inorderNodes);
+            int weight = 0;
+            foreach(var node in inorderNodes)
+            {
+                if (!node.IsLeafNode) continue;
+                weight += node.StrFrag.Length;
+            }
+            Rope concatedRope = new Rope(weight);
+            concatedRope.Head.LeftChild = left.Head;
+            concatedRope.Head.RightChild = right.Head;
+            return concatedRope;
         }
 
         private (Node,Node) Split(Node node, int position)
